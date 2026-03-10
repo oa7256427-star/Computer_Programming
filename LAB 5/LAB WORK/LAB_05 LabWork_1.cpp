58b5f22d938155df2107e434589d2b2f7170a4a4
Awais Khan <oa7256427@gmail.com>
2026-03-10 22:02:25 +0500

    Add files via upload

---
+++ LAB 5/LAB WORK/LAB_05 LabWork_1.cpp
@@ -0,0 +1,22 @@
+// Name: Awais Khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0734
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
+
+
---
+++ LAB 5/LAB WORK/LAB_05 LabWork_2.cpp
@@ -0,0 +1,23 @@
+// Name: Awais Khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0734
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    }
+    while(i <= 10);
+
+    return 0;
+}
+
+
