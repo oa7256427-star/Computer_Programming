8e7722ca3e3b8ab4b4c402d3b6887294bf96b418
Awais Khan <oa7256427@gmail.com>
2026-03-10 21:11:04 +0500

    Add files via upload

---
+++ LAB 3/LAB TASK/LAB_3 DRILLTASK_07.cpp
@@ -0,0 +1,25 @@
+// Name: Awais Khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0734
+// Section: A
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float base, perpendicular, hypotenuse;
+
+    cout << "Enter base: ";
+    cin >> base;
+
+    cout << "Enter perpendicular: ";
+    cin >> perpendicular;
+
+    hypotenuse = sqrt(base*base + perpendicular*perpendicular);
+
+    cout << "Hypotenuse = " << hypotenuse << endl;
+
+    return 0;
+}
+
