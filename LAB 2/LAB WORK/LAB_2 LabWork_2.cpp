0a42887efd97ba9c93bcba2952b346b9805547b7
Awais Khan <oa7256427@gmail.com>
2026-03-10 21:08:14 +0500

    Add files via upload

---
+++ LAB 2/LAB WORK/LAB_2 LabWork_2.cpp
@@ -0,0 +1,15 @@
+// Name: Awais Khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0734
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14;  // declare float variable pi
+    cout << "Value of pi = " << pi << endl;  // display the value
+    return 0;
+}
+
+
